6a446e444e715f6fae1f990a15bf5bac43dd61de
NaranjaX <naranjax@MAC-68096.local>
2023-05-03 19:50:06 -0300

    realice cambios en el carrito

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CarritoProvider } from './components/Context/CarritoContext';
+import Cart from './components/Cart/Cart';
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <CarritoProvider>
         <NavBar />
         <Routes>
           <Route path='/' element={ <ItemListContainer greeting= {"Conocé toda la colección"}/> } />
           <Route path='/categoria/:idCategoria' element={ <ItemListContainer/> }/>
           <Route path='/Item/:itemId' element={ <ItemDetailContainer/> }/>
+          <Route path="/Cart" element={<Cart/>} />
+          <Route path="*" element={<h2> Sitio en construccion </h2>} />
         </Routes>
-        
-      
+
+      </CarritoProvider>
       </BrowserRouter>
 
-</>
+    </>
+
  );
 }
 
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import './CartWidget.css'
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { CarritoContext } from '../Context/CarritoContext';
 
 const CartWidget = () => {
+    const {carrito} = useContext(CarritoContext);
+    const totalCantidad = carrito.reduce((total, producto) => total + producto.cantidad, 0);
     const imgCarrito = "https://cdn-icons-png.flaticon.com/128/3649/3649614.png"
+
   return (
-    <div>
+    <Link to='/Cart'>
         <img className='imgCarrito' src = {imgCarrito}  alt="Carrito" />
-        <strong className='numero'>5</strong>
-    </div>
+        {
+          totalCantidad
+        }
+    </Link>
   )
 }
 
